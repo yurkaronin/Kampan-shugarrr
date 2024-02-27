4a0d9696d70a2c949ad3b46d241a099c09bf0f4d
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-02-27 15:45:17 +0300

    +

--- js/parts/sliders.js
+++ js/parts/sliders.js
@@ -2,12 +2,31 @@
 if (document.querySelector('.team .swiper')) {
 
   var swiperTeam = new Swiper(".team .swiper", {
+    // Параметры по умолчанию
     slidesPerView: 6,
     spaceBetween: 32,
-    // freeMode: true,
     navigation: {
       nextEl: ".team .swiper-button-next",
       prevEl: ".team .swiper-button-prev",
     },
+    // Адаптивные настройки
+    breakpoints: {
+      // при ширине 1500px и меньше
+      1500: {
+        slidesPerView: 6,
+        spaceBetween: 32
+      },
+      // при ширине 1023px и меньше
+      1023: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      },
+      // при ширине 1022px и меньше
+      1022: {
+        slidesPerView: 3,
+        spaceBetween: 10
+      }
+    }
   });
+
 };
